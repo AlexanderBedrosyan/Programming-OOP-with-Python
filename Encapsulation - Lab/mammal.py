aaba341d3a4df8e99270d94d785d7ff16cdea773
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-11-20 15:17:00 +0200

    Solutions for Encapsulation - Lab
    Solutions for Encapsulation - Lab

---
+++ Encapsulation - Lab/mammal.py
@@ -0,0 +1,22 @@
+class Mammal:
+
+    def __init__(self, name: str, type: str, sound: str):
+        self.name = name
+        self.type = type
+        self.sound = sound
+        self.__kingdom = 'animals'
+
+    def make_sound(self) -> str:
+        return f"{self.name} makes {self.sound}"
+
+    def get_kingdom(self) -> str:
+        return self.__kingdom
+
+    def info(self) -> str:
+        return f"{self.name} is of type {self.type}"
+
+
+mammal = Mammal("Dog", "Domestic", "Bark")
+print(mammal.make_sound())
+print(mammal.get_kingdom())
+print(mammal.info())
