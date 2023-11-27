e494b0713b5104fe15cc29a981542e4f67e8136c
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-11-27 15:00:17 +0200

    Solutions for Polymorphism and Abstraction - Exercise
    Solutions for Polymorphism and Abstraction - Exercise

---
+++ Polymorphism and Abstraction - Exercise/vehicle.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def drive(self, distance):
+        pass
+
+    @abstractmethod
+    def refuel(self, fuel):
+        pass
+
+
+class Car(Vehicle):
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        fuel_needed = distance * (self.fuel_consumption + 0.9)
+        if self.fuel_quantity >= fuel_needed:
+            self.fuel_quantity -= fuel_needed
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        fuel_needed = distance * (self.fuel_consumption + 1.6)
+        if self.fuel_quantity >= fuel_needed:
+            self.fuel_quantity -= fuel_needed
+
+    def refuel(self, fuel):
+        self.fuel_quantity += 0.95 * fuel
+
+
+car = Car(20, 5)
+car.drive(3)
+print(car.fuel_quantity)
+car.refuel(10)
+print(car.fuel_quantity)
+truck = Truck(100, 15)
+truck.drive(5)
+print(truck.fuel_quantity)
+truck.refuel(50)
+print(truck.fuel_quantity)
+
