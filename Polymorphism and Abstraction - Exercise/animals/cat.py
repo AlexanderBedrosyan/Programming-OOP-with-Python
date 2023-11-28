764a3f354c8d6de7c7b16af8766a029a1b88641b
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-11-28 09:11:46 +0200

    Solutions for Polymorphism and Abstraction - Exercise
    Solutions for Polymorphism and Abstraction - Exercise

---
+++ Polymorphism and Abstraction - Exercise/animals/cat.py
@@ -0,0 +1,10 @@
+from animals.animal import Animal
+
+
+class Cat(Animal):
+
+    def __repr__(self):
+        return f"This is {self.name}. {self.name} is a {self.age} year old {self.gender} {self.__class__.__name__}"
+
+    def make_sound(self):
+        return f"Meow meow!"
---
+++ Polymorphism and Abstraction - Exercise/animals/kitten.py
@@ -0,0 +1,10 @@
+from animals.cat import Cat
+
+
+class Kitten(Cat):
+
+    def __init__(self, name, age,):
+        super().__init__(name, age, 'Female')
+
+    def make_sound(self):
+        return 'Meow'
