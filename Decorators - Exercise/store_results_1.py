04bad2c9d02a92421b0117b46a8d482aaa58aefe
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-12-08 14:17:45 +0200

    Solutions for Decorators - Exercise
    Solutions for Decorators - Exercise

---
+++ Decorators - Exercise/store_results_1.py
@@ -0,0 +1,22 @@
+class store_results:
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        with open("result.txt", "a",) as f:
+            f.write(f"Function '{self.func.__name__}' was called. Result: {self.func(*args)}\n")
+
+
+@store_results
+def add(a, b):
+    return a + b
+
+
+@store_results
+def mult(a, b):
+    return a * b
+
+
+add(2, 2)
+mult(6, 4)
